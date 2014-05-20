50c500f7b3374d2849378c5174804ca0f84bb529
Jonny Matthews <user@users-MacBook.local>
2014-05-20 13:37:56 +0100

    added a test class

---
+++ JMTest.h
@@ -0,0 +1,13 @@
+//
+//  JMTest.h
+//  Git Test
+//
+//  Created by Jonny Matthews on 20/05/2014.
+//  Copyright (c) 2014 Jonny Matthews. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JMTest : NSObject
+
+@end
---
+++ JMTest.m
@@ -0,0 +1,13 @@
+//
+//  JMTest.m
+//  Git Test
+//
+//  Created by Jonny Matthews on 20/05/2014.
+//  Copyright (c) 2014 Jonny Matthews. All rights reserved.
+//
+
+#import "JMTest.h"
+
+@implementation JMTest
+
+@end
